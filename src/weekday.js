66b173b72bdd670b5cde16162f596e77762b0a02
Ian Scott <chesnekov@gmail.com>
2020-06-24 11:00:05 -0700

    adds logic to return the month number and pass respective test
    Co-Authored-By: Ben Russell <benjaminrussell36@gmail.com>

--- src/weekday.js
+++ src/weekday.js
@@ -2,14 +2,17 @@ export class Weekday {
   
   constructor(dateString) {
     this.dateString = dateString;
-
+    this.parseDate();
+    
+    this.month
     this.day;
-    this.month;
     this.year;
 
     this.dayOfWeek;
   }
 
-  
-
+  parseDate() {
+    const parsedDates = this.dateString.split("/");
+    this.month = parseInt(parsedDates[0]);
+  }
 };
